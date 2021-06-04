b8877b07489c2bc58fd55e05cde107723c8e9ce9
osmansufy <osmansufy20@gmail.com>
2021-06-04 22:50:34 +0600

    footer-done

--- src/components/Activities/RecentActivities/RecentActivities.js
+++ src/components/Activities/RecentActivities/RecentActivities.js
@@ -15,7 +15,7 @@ const RecentActivities = () => {
     <div>
       <h4>Recent Activities</h4>
 
-      <div className="allActivitiesWrapper bg-white">
+      <div className="allActivitiesWrapper px-4 pt-2 pb-5 bg-white">
         <div className="d-flex justify-content-between">
           <div className="navWrapper">
             <Nav variant="pills" defaultActiveKey="link-1">
@@ -35,20 +35,30 @@ const RecentActivities = () => {
           <Nav.Link>See all</Nav.Link>
         </div>
         <div className="listActivities">
+          <h6 className="font-weight-normal py-4 border-bottom">
+            Today 15th june
+          </h6>
           <ul className="list-group list-group-flush">
             {approvedActivities.length > 0 &&
               approvedActivities.map((act) => (
-                <li className="list-group-item">
-                  <img src={`images/${act.profile}`} alt="" />
-                  <div className="actInfo">
-                    <p>Dollar Balance (Primary)</p>
-                    <p className="text-muted">
-                      to{act.name}.{act.email}
-                    </p>
+                <li className="list-group-item d-flex justify-content-between">
+                  <div className="flex-grow-1 d-flex">
+                    <div className="d-flex mr-3 justify-content-between align-items-center">
+                      <img src={`images/${act.profile}`} alt="" />
+                    </div>
+                    <div className="actInfo">
+                      <p>Dollar Balance (Primary)</p>
+                      <p className="text-muted">
+                        to{act.name}.{act.email}
+                      </p>
+                    </div>
                   </div>
-                  <div className="amountInfo">
-                    +${act.amount}
-                    <Button variant="outline-dark">{act.status}</Button>
+
+                  <div className="amountInfo d-flex flex-column">
+                    <h4>+${act.amount}</h4>
+                    <Button className="py-1 px-3" variant="outline-dark">
+                      {act.status}
+                    </Button>
                   </div>
                 </li>
               ))}
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,20 +7,26 @@ const Footer = () => {
   return (
     <section className="footer">
       <Container>
-        <div className="row">
+        <div className="row bg-white">
           <div className="col-md-6">
-            <div className="appInfoWrapper">
+            <div className="appInfoWrapper pl-5 pt-5">
               <h5>Get Fintech app</h5>
               <p>Available on</p>
               <div className="appsWrapper d-flex">
-                <img src={playStoreImg} alt="" />
-                <img src={iStore} alt="" />
+                <div className="appImg d-flex mr-3">
+                  <img src={playStoreImg} alt="" />
+                </div>
+                <div className="appImg d-flex">
+                  <img src={iStore} alt="" />
+                </div>
               </div>
             </div>
           </div>
           <div className="col-md-6">
-            <div className="footer-banner">
-              <img src={iPhone} alt="" />
+            <div className="footer-banner pt-5">
+              <div className="footerPhoneImg">
+                <img src={iPhone} alt="" />
+              </div>
             </div>
           </div>
         </div>
